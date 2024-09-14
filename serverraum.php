c08017b42add30c498f2717916188cff8f85bb2c
Simon <czechak@gmx.de>
2024-09-14 17:23:42 +0200

    g

---
+++ serverraum.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Serverraumkosten</title>
+    </head>
+
+    <body>
+        <?php
+            $Verbrauch = 18000.0; //in kWh
+            $Grundpreis = 24.95; //in Euro
+            $Arbeitspreis = 18.5; //in Cent
+            
+            $Jahrsbruttokosten = $Verbrauch*$Arbeitspreis/100+$Grundpreis*12;
+            echo number_format($Jahrsbruttokosten,2,","," ");
+        ?>
+    </body>
+</html>
