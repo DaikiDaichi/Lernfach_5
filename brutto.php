5dd7fbc4db73cbb7dd5d2a008476d6d06c74cda3
Simon <czechak@gmx.de>
2024-09-14 12:30:04 +0200

    new file

---
+++ brutto.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Brutto</title>
+    </head>
+
+    <body>
+        <?php
+            $mwst = 19.0;
+            $net_kosten = 249.0;
+
+            $mwst_betrag = $mwst / 100 * $net_kosten;
+            $brutto_kosten = $net_kosten + $mwst_betrag;
+            echo "Bruttokosten: ", $brutto_kosten, "<br> MWST-Betrag: ", $mwst_betrag;
+        ?>
+    </body>
+</html>
