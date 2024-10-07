aeffe19b4f1cc8de1465679337a0c0362d22e139
Simon <czechak@gmx.de>
2024-10-07 21:12:19 +0200

    Seite 42 nr3

--- forms/basic_logic/automatisch/stromtarif.php
+++ forms/basic_logic/automatisch/stromtarif.php
@@ -6,19 +6,36 @@
     <body>
         <?php
             $verbrauch = $_GET["verbrauch"];
-            $t1 = null;
-            $t2 = null;
-            $t3 = null;
+            $t1 = 2.97*12+$verbrauch*31.5/100;
+            $t2 = 5.94*12+$verbrauch*28.9/100;
+            $t3 = 11.84*12+$verbrauch*26.5/100;
+
+            $t1 = round($t1,2);
+            $t2 = round($t2,2);
+            $t3 = round($t3,2);
 
             if ($t3 <= $t2 and $t3 <= $t1) {
-                echo"pass";
+                echo"Bester Tarif ist T3";
+                $best_t = $t3;
             }
-            elseif ($t1 <= $t2) {
-                echo"pass";
+            elseif ($t2 <= $t1) {
+                echo"Bester Tarif ist T2";
+                $best_t = $t2;
             }
             else {
-                echo"pass";
+                echo"Bester Tarif ist T1";
+                $best_t = $t1;
             }
+
+            $mwst_betrag = round($best_t/119*19, 2);
+            $net_kost = round($best_t-$mwst_betrag, 2);
+
+            echo"<br>Brutto:    $best_t<br>
+                Netto:  $net_kost<br>
+                Mwst:   $mwst_betrag";
+
+            //number_format scheint eine Zahl zum string abzuändern. numberformat nur bei Ausgaben verwenden!
+            //(int)$var oder (float)$var kann das ergbniss zurückformen, aber nur ohne kommastellen!
         ?>
     </body>
 </html>
